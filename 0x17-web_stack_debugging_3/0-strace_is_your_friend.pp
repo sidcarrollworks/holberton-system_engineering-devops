161983c5afd9c5508d0bc0a3483b358bb6e4ef75
squidcarroll <squidcarroll@gmail.com>
2018-05-16 14:06:05 -0700

    add only if

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #replace extension for file
 exec { 'fix-wordpress':
-  command  => 'sed -i s/phpp/php/ /var/www/html/wp-settings.php',
-  path     => '/usr/bin/',
-  provider => 'shell'
+  command => 'sed -i s/phpp/php/ /var/www/html/wp-settings.php',
+  path    => '/usr/bin/',
+  onlyif  => 'test -e /var/www/html/wp-settings.php'
 }
